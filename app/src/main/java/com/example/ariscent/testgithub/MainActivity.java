1a00ff2252bca274366e737d1f0b907df08de810
rakesh1979 <rakeshram1979@GMAIL.COM>
2016-12-08 11:19:31 +0530

    update 1

--- app/src/main/java/com/example/ariscent/testgithub/MainActivity.java
+++ app/src/main/java/com/example/ariscent/testgithub/MainActivity.java
@@ -19,6 +19,8 @@ public class MainActivity extends AppCompatActivity {
 
         int i = 1 + 1;
 
+
+
     }
 
 
---
+++ app/src/main/java/com/example/ariscent/testgithub/Test.java
@@ -0,0 +1,16 @@
+package com.example.ariscent.testgithub;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Ariscent on 12/08/16.
+ */
+
+public class Test extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
