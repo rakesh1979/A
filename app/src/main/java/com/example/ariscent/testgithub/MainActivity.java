51038c6de535c89ae58dc782e324c25ff5b4c184
rakesh1979 <rakeshram1979@GMAIL.COM>
2016-12-08 11:14:07 +0530

    update 1

--- app/src/main/java/com/example/ariscent/testgithub/MainActivity.java
+++ app/src/main/java/com/example/ariscent/testgithub/MainActivity.java
@@ -9,6 +9,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TestUpload();
+
     }
 
 
@@ -17,5 +20,7 @@ public class MainActivity extends AppCompatActivity {
         int i = 1 + 1;
 
     }
+
+
 
 }
